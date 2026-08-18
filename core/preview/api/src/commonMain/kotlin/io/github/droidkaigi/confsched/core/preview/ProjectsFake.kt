4b1398d5e494e8a46ff1a482571b35d3e2509a92
Jihyun-Park0 <jihyun.park.private@gmail.com>
2026-08-18 17:20:13 +0900

    Fix EventMapScreenPresenterTest & Update ProjectsFake

--- core/preview/api/src/commonMain/kotlin/io/github/droidkaigi/confsched/core/preview/ProjectsFake.kt
+++ core/preview/api/src/commonMain/kotlin/io/github/droidkaigi/confsched/core/preview/ProjectsFake.kt
@@ -11,23 +11,44 @@ fun Projects.Companion.fake(): Projects = Projects(
     items = persistentListOf(
         Project(
             id = ProjectId("1"),
-            title = MultiLangText(ja = "Meetup（ランチタイム）", en = "Meetup (Lunchtime)"),
-            i18nDesc = MultiLangText(ja = "Description Ja", en = "Description En"),
+            title = MultiLangText(
+                ja = "Meetup（ランチタイム）",
+                en = "Meetup (Lunchtime)",
+            ),
+            i18nDesc = MultiLangText(
+                ja = "様々なテーマごとに集まって、一緒にランチを食べながらお話ししましょう。席에限りがありますので、お弁当受け取り後お早めにお越しください。",
+                en = "Gather around a topic and chat over lunch. Seats are limited, so please come soon after picking up your bento.",
+            ),
             room = Room.NARWHAL,
-            message = MultiLangText(ja = "Message Ja", en = "Message En"),
+            message = MultiLangText(
+                ja = "※こちらのイベントは時間が変更されました",
+                en = "* The time of this event has changed",
+            ),
             moreDetailsUrl = "https://droidkaigi.jp/2026/",
         ),
         Project(
             id = ProjectId("2"),
-            title = MultiLangText(ja = "キャリア相談会", en = "Career Counseling"),
-            i18nDesc = MultiLangText(ja = "Description Ja", en = "Description En"),
+            title = MultiLangText(
+                ja = "キャリア相談会",
+                en = "Career Counseling",
+            ),
+            i18nDesc = MultiLangText(
+                ja = "様々なテーマごとに集まって、一緒にランチを食べながらお話ししましょう。席에限りがありますので、お弁当受け取り後お早めにお越しください。",
+                en = "Gather around a topic and chat over lunch. Seats are limited, so please come soon after picking up your bento.",
+            ),
             room = Room.OTTER,
             moreDetailsUrl = "https://droidkaigi.jp/2026/",
         ),
         Project(
             id = ProjectId("3"),
-            title = MultiLangText(ja = "コミュニケーションエリア", en = "Communication Area"),
-            i18nDesc = MultiLangText(ja = "Description Ja", en = "Description En"),
+            title = MultiLangText(
+                ja = "コミュニケーションエリア",
+                en = "Communication Area",
+            ),
+            i18nDesc = MultiLangText(
+                ja = "様々なテーマごとに集まって、一緒にランチを食べながらお話ししましょう。席에限りがありますので、お弁当受け取り後お早めにお越しください。",
+                en = "Gather around a topic and chat over lunch. Seats are limited, so please come soon after picking up your bento.",
+            ),
             room = Room.QUAIL,
         ),
     ),
