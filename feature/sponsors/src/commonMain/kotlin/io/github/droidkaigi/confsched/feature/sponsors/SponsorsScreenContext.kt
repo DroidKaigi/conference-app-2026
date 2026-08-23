99ad5f7e813e1fbf2285daf318a23d857ed34eaf
kitakkun <48154936+kitakkun@users.noreply.github.com>
2026-08-23 15:59:07 +0900

    Carry a logger on both role contexts

--- feature/sponsors/src/commonMain/kotlin/io/github/droidkaigi/confsched/feature/sponsors/SponsorsScreenContext.kt
+++ feature/sponsors/src/commonMain/kotlin/io/github/droidkaigi/confsched/feature/sponsors/SponsorsScreenContext.kt
@@ -2,17 +2,19 @@ package io.github.droidkaigi.confsched.feature.sponsors
 
 import dev.zacsweers.metro.Inject
 import dev.zacsweers.metro.SingleIn
+import io.github.droidkaigi.confsched.core.common.KaigiLogger
 import io.github.droidkaigi.confsched.core.common.PresenterContext
 import io.github.droidkaigi.confsched.core.common.ScreenContext
 import io.github.droidkaigi.confsched.core.model.SponsorsQueryKey
 import io.github.droidkaigi.confsched.core.model.SponsorsScreenScope
 
 @Inject
-class SponsorsPresenterContext : PresenterContext
+class SponsorsPresenterContext(override val logger: KaigiLogger) : PresenterContext
 
 @Inject
 @SingleIn(SponsorsScreenScope::class)
 class SponsorsScreenContext(
     val sponsorsQueryKey: SponsorsQueryKey,
+    override val logger: KaigiLogger,
     val presenterContext: SponsorsPresenterContext,
 ) : ScreenContext
