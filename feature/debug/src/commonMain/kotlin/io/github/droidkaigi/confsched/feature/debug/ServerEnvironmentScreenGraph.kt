21da82eae87311fb163ceb25598aa948caed7926
kitakkun <48154936+kitakkun@users.noreply.github.com>
2026-08-26 01:34:09 +0900

    Reject unused imports with ktlint

--- feature/debug/src/commonMain/kotlin/io/github/droidkaigi/confsched/feature/debug/ServerEnvironmentScreenGraph.kt
+++ feature/debug/src/commonMain/kotlin/io/github/droidkaigi/confsched/feature/debug/ServerEnvironmentScreenGraph.kt
@@ -3,7 +3,6 @@ package io.github.droidkaigi.confsched.feature.debug
 import dev.zacsweers.metro.ContributesTo
 import dev.zacsweers.metro.GraphExtension
 import dev.zacsweers.metro.Provides
-import io.github.droidkaigi.confsched.core.common.Navigator
 import io.github.droidkaigi.confsched.core.common.UiScope
 import io.github.droidkaigi.confsched.core.model.MutationTag
 import io.github.droidkaigi.confsched.core.model.ServerEnvironmentScreenScope
