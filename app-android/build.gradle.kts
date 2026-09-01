fc179b2336d077a75cf5e3f24e5f9043022f0406
kitakkun <48154936+kitakkun@users.noreply.github.com>
2026-09-02 02:48:18 +0900

    Bump the app version to 1.2.0

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "io.github.droidkaigi.confsched2026"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = libs.versions.droidkaigiApp.get()
     }
 
